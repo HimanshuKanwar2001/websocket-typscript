ae33308bb606d4a07fff4e488fe5bf5f4fd5c89e
himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-06-29 21:57:43 +0530

    prisma week 18.1

--- week-18/backend/src/index.ts
+++ week-18/backend/src/index.ts
@@ -1,1 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+
+
+const client = new PrismaClient();
+
+async function createUser() {
+  await client.user.create({
+    data: {
+      username: "himanshu",
+      email: "himanshu2001@gmail.com",
+      password: "1234",
+      age: 23,
+      city: "delhi",
+    },
+  });
+}
+
+createUser();
+
 console.log("hi there");
